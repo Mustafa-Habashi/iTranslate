23bd818ad32449ec89d0782b609a5b6c5ee8fc37
Mustafa-Habashi <mustafahabashi10@gmail.com>
2023-01-03 16:41:49 +0000

    Created an input box and Language select option for user

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <h2 class="text-center">iTranslate</h2>
+      <div>
+        <div class="mb-3">
+          <label for="text" class="form-label">
+            Enter your Text:
+          </label>
+          <textarea class="form-control" id="bio" rows="3"></textarea>
+        </div>
+        <div class="col-12">
+          <button class="btn btn-primary" type="submit">
+            Register
+          </button>
+        </div>
+        <div><form action="/action_page.php">
+          <label for="cars">Select a Language:</label>
+          <select name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+
+        </form></div>
+
+        <br></br>
+        <div class="mb-3">
+          <label for="result" class="form-label">
+            Translation
+          </label>
+          <textarea class="form-control" id="result" rows="3"></textarea>
+        </div>
+
+      </div>
     </div>
   );
 }
