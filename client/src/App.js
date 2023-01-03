9e14e769e4b36d5f751151adb24f30f966aa4f86
Mustafa-Habashi <mustafahabashi10@gmail.com>
2023-01-03 17:00:42 +0000

    successfuly managed to send api request to endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,28 @@
-
+import axios from "axios";
 
 function App() {
+
+  const translate = () => {
+    const options = {
+      method: 'GET',
+      url: process.env.REACT_APP_RapidAPI_URL,
+      params: { text: 'Hello, world!!', to: 'ar', from: 'en' },
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RapidAPI_Key,
+        'X-RapidAPI-Host': process.env.REACT_APP_RapidAPI_Host
+      }
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      console.log(response.data.translated_text);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+  }
+
+
   return (
     <div className="container mt-3">
       <h2 class="text-center">iTranslate</h2>
@@ -12,17 +34,18 @@ function App() {
           <textarea class="form-control" id="bio" rows="3"></textarea>
         </div>
         <div class="col-12">
-          <button class="btn btn-primary" type="submit">
-            Register
+          <button class="btn btn-primary"
+            onClick={translate}>
+            Translate!!
           </button>
         </div>
         <div><form action="/action_page.php">
           <label for="cars">Select a Language:</label>
           <select name="cars" id="cars">
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="opel">Opel</option>
-            <option value="audi">Audi</option>
+            <option value="ar">Arabic</option>
+            <option value="es">Spanish</option>
+            <option value="fr">French</option>
+            <option value="de">German</option>
           </select>
 
         </form></div>
